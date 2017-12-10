7ec3f80305ed8fc59f3267f5c70adf1b133d4227
inpos <inpos@yandex.ru>
2017-12-10 16:53:51 +0300

    PostgreSQL support

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `defense` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `epoch` int(11) NOT NULL,
   `type` tinyint(4) NOT NULL,
-  `ipaddr` varchar(40) NOT NULL,
+  `ipaddr` varchar(256) NOT NULL,
   `data` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
